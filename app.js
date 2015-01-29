c93ef1d10b5c1fad15c1404564c1ef5db5c36c2b
SamiErrighi <sami.errighi@gmail.com>
2015-01-29 10:54:33 +0100

    fix mongodb

--- app.js
+++ app.js
@@ -14,12 +14,15 @@ var mongoose = require('mongoose');
 var app = express();
 
 
-if (app.get('env') === 'development') {
-    mongoose.connect(env.development.db);
-}
- if (process.env.ENV === 'HEROKU'){
-    mongoose.connect(env.production.db);
-}
+var mongolabStringConnexion = process.env.MONGO_STRING_CONNECTION || 'mongodb://localhost:27017/ws-restapi';
+
+mongoose.connect(mongolabStringConnexion);
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback () {
+    console.log('Connexion establish to ' + mongolabStringConnexion);
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
